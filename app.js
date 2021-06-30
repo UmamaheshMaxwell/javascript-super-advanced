5bbe56a94369a35647cb0bbffd10e473f73bc469
UmamaheshMaxwell <maheshmeka16@gmail.com>
2021-06-30 11:19:20 +0530

    added basic files

---
+++ app.js
@@ -0,0 +1,20 @@
+var a =10
+var a;
+console.log(a)
+sample()
+
+function sample(){
+    console.log('Hello')
+}
+
+
+console.log(b)
+
+var b;
+
+/* hoisting
+? step1: 
+? scan through variables and functions
+will keep variables and function in memoery
+it will assign a defaut value called undefined
+*/
