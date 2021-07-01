ffc36353c2472abc7c36a2130bdb75647ca73876
UmamaheswararaoMeka <maheshmeka16@gmail.com>
2021-07-01 09:33:30 +0530

    added functions

---
+++ 5.functions/1.regular-function.js
@@ -0,0 +1,21 @@
+// function sample(){
+//    // return 1
+//    //return 
+//    //retrun "string"
+//    // return true 
+//    //return function(){}
+//    return []
+// }
+
+// console.log(sample())
+
+function getFullName(firstName, lastName){
+    return firstName + ' ' + lastName
+}
+
+console.log(getFullName())
+console.log(getFullName('Uma'))
+console.log(getFullName('Scott', 'Desatnick'))
+console.log(getFullName('Scott', 'Desatnick','manager'))
+
+
